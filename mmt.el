558c7f94304c3021460a01f729d31e3578921946
Matthew Trentacoste <mt@matttrent.com>
2011-02-25 04:51:56 -0800

    No more fiddling for now

--- mmt.el
+++ mmt.el
@@ -1,29 +1,51 @@
 
+;; --- EXISTING ----------------------------------------------------------------
+
 (add-to-list 'load-path (concat dotfiles-dir "/vendor"))
-;(add-to-list 'load-path (concat dotfiles-dir "/topfunky"))
 (add-to-list 'load-path (concat dotfiles-dir "/mtrent"))
 
 (load (concat dotfiles-dir "topfunky.el"))
-;(require 'topfunky)
 
-;(add-to-list 'load-path (concat dotfiles-dir "/vendor/textmate.el"))
-;(require 'textmate)
+;; --- PEEPOPEN ----------------------------------------------------------------
+
 (require 'peepopen)
-;(textmate-mode)
 (setq ns-pop-up-frames nil)
 
+;; --- MAXFRAME ----------------------------------------------------------------
+
 (require 'maxframe)
-(setq mf-display-padding-width 50)
+(setq mf-offset-x 40)
 (add-hook 'window-setup-hook 'maximize-frame t)
 
+;; --- REVBUFS -----------------------------------------------------------------
+
 (require 'revbufs)
+
+;; --- EDITTING ----------------------------------------------------------------
+
+;(tool-bar-mode 1)
+(menu-bar-mode 1)
+
+(global-set-key "\M- " 'set-mark-command)
+
+(global-set-key [(super shift left)] 'previous-multiframe-window)
+(global-set-key [(super shift right)] 'next-multiframe-window)
+
+;; --- ORG MODE ----------------------------------------------------------------
+
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+
+(setq org-log-done t)
+
+;; --- TEXT APPEARANCE ---------------------------------------------------------
+
+;; --- Theme
 
 (load (concat dotfiles-dir "mtrent/twilight.el"))
 (color-theme-twilight)
 
-(setq mac-allow-anti-aliasing nil)
-
-(global-set-key (kbd "M- ") 'set-mark-command)
+;; --- PROGRAMMING -------------------------------------------------------------
 
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
@@ -49,13 +71,4 @@
      ("\\.hxx\\'" (".cxx")))))
  '(javascript-indent-level 2))
 
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil 
-                         :inverse-video nil :box nil
-                         :strike-through nil :overline nil :underline nil
-                         :slant normal :weight normal :height 110 :width normal
-                         :foundry "apple" :family "Menlo")))))
+
